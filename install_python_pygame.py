6946cd497a3c57a6fb3b75417cbad10a19dfe60a
Aroun Le BriCodeur <mc.family3531@gmail.com>
2019-03-23 00:29:28 +0100

    installation de python et pygame
    instructions pour l'installation de python et pygame

---
+++ install_python_pygame.py
@@ -0,0 +1,19 @@
+// install python
+1) télécharger la dernière version de python sur :
+	https://www.python.org/
+2) dans le terminal mettre les commandes suivantes :
+cibler le dossier de python :
+	cd Bureau/DossierPython
+une fois dans le dossier python :
+	./configure
+une fois le 1er chargement effectué :
+	make	
+une fois le 2eme chargement effectué :	
+	su root
+	make install
+Et voilà plus qu'à ouvrir un nouveau terminal et taper :
+	python
+pour vérifier la bonne installation de python
+
+// install pygame
+sudo apt-get install python-pygame
